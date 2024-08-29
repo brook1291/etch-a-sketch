778e20c3ee37d420d8164ac768e26529b3cd0391
DOP <167078399+brook1291@users.noreply.github.com>
2024-08-29 08:27:05 +0100

    Generate the grid of square divs when the page loads

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const container = document.querySelector('#container');
+
+window.onload = () => {
+  for (let i = 1; i <= 256; i++) {
+    const squareDiv = document.createElement('div');
+    container.appendChild(squareDiv);
+  }
+};
