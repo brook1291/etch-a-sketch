dae322ad8dd89fdbbb96f8abc7c468bd516b6335
DOP <167078399+brook1291@users.noreply.github.com>
2024-08-29 08:54:16 +0100

    Add hover effect to each div and change grid button

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const container = document.querySelector('#container');
+const changeGridBtn = document.querySelector('#change-grid');
 
-window.onload = () => {
-  for (let i = 1; i <= 256; i++) {
-    const squareDiv = document.createElement('div');
-    container.appendChild(squareDiv);
-  }
-};
+for (let i = 1; i <= 256; i++) {
+  const squareDiv = document.createElement('div');
+  container.appendChild(squareDiv);
+  squareDiv.addEventListener('mouseover', () => {
+    squareDiv.classList.add('color');
+  });
+}
